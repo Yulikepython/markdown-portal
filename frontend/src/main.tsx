e2a7e1e780e0a7185d5ca66368be0ee46a3c7464
Hiroshi Nishito <admin@itc.tokyo>
2025-01-04 21:25:20 +0900

    #6 amplifyConfigureに設定ファイルを統合
    散らばっていた定数・変数を統合

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from "react-oidc-context";
 
 import './index.css'
 import App from './App.tsx'
 
-const cognitoAuthConfig = {
-    authority: `https://${import.meta.env.VITE_COGNITO_DOMAIN}.auth.ap-northeast-1.amazoncognito.com`,
-    client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
-    redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
-    response_type: "code",
-    scope: "email openid phone",
-};
+import './config/amplifyConfigure.ts';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <AuthProvider {...cognitoAuthConfig}>
-            <App />
-        </AuthProvider>
+        <App />
     </StrictMode>
 )
