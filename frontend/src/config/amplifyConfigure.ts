5163356411f0f26f1bab89eb74fa2739df4c00ac
Hiroshi Nishito <admin@itc.tokyo>
2025-01-09 23:35:51 +0900

    bugfix] cognitoログインボタンが機能しない

--- frontend/src/config/amplifyConfigure.ts
+++ frontend/src/config/amplifyConfigure.ts
@@ -1,12 +1,11 @@
 import { ResourcesConfig, Amplify } from "aws-amplify";
-import { apiBaseUrl} from "../services/apiClient.ts";
 
 const COGNITO_REGION = import.meta.env.VITE_COGNITO_REGION;
 const COGNITO_USER_POOL_ID = import.meta.env.VITE_COGNITO_USER_POOL_ID;
 const COGNITO_USER_POOL_CLIENT = import.meta.env.VITE_COGNITO_CLIENT_ID;
 const COGNITO_DOMAIN_PREFIX = import.meta.env.VITE_COGNITO_DOMAIN;
-const SIGNIN_URL = apiBaseUrl;
-const SIGNOUT_URL = apiBaseUrl;
+const SIGNIN_URL = import.meta.env.VITE_SIGNIN_URL || "http://localhost:5173";
+const SIGNOUT_URL = import.meta.env.VITE_SIGNOUT_URL || "http://localhost:5173";
 
 export const config: ResourcesConfig = {
     Auth: {
